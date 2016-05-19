c1f8f663e8f277a687d2495f0ff162793590d548
iwj <jentlewoo@gmail.com>
2016-05-19 16:40:18 +0800

    sync old items

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ FROM http://cn.bing.com
 <a href="https://raw.githubusercontent.com/iwj/BingWow/master/alpha/BingWow.exe">BingWow.exe</a>
 
 使用方法：双击，即可直接下载源图到当前目录。
+
+#TODO
+
+目前将主要相对于 Windows 而言
+
+1)自动识别系统是32位还是64位
+
+2)本地的定时任务。比如开机后几分钟后自动运行下载今日美图，开机后不一定就是联网的，比如有些电脑需要拨号上网：）
+
+3)运行在网络云端的脚本，配合 IFTTT 符合某个条件比如自己中意的图则触发。
+
+等等更多有趣用法。
--- main.py
+++ main.py
@@ -7,38 +7,9 @@ import requests
 import json
 import urllib
 
-def get_soup(url, file_str):
-    request_result = requests.get(url)
-    soup = bs4.BeautifulSoup(request_result.text, "html.parser")
-    #soup = bs4.BeautifulSoup("<html>data</html>", "html.parser")
-
-    error_img = "http://www.geekpark.net/404_bg.png"
-
-    if soup.img:
-        file_str = file_str + "\n" + soup.img["src"]
-        print soup.img["src"]
-        
-    else:
-        print "Not find image"
-        print error_img
-
-    return file_str
-
-def run_wufazhuce():
-    file_str = ""
-    url_pre = "http://wufazhuce.com/one/vol."
-    #TODO 计算今天与第一期前一天的差，得到今天的期数
-    #article_number = 1098
-    for i in range(1, 1099):
-        article_number = i
-        url = url_pre + str(article_number)
-        file_str = get_soup(url, file_str)
-
-    #若不存在，自动新建文件
-    with open("one_list.txt", "w") as f:
-        f.write(file_str)
-
-def run_bing():
+def bing_wow():
+    #error_img_url
+    #error_img = "http://www.geekpark.net/404_bg.png"
     bing_json = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1444634662901&pid=hp"
     result = requests.get(bing_json).text
     result_dict = json.loads(result)
@@ -46,20 +17,11 @@ def run_bing():
     file_name = img_url.split("/")[-1]
     urllib.urlretrieve(img_url, file_name)
     img_status =  img_url+"\nsuccess"
-    log_text = "* " * 10
-    log_text += "\n抓取日志\n"
-    log_text += " success"
+    log_text = "抓取日志"
+    log_text += "\n success"
     with open("bing_log.txt", "w") as f:
         f.write(log_text)
 
-def main():
-    choice = raw_input("输入选择（A:ONE一个 或者B:Bing）")
-    if choice == "A":
-        run_wufazhuce()
-    elif choice == "B":
-        run_bing()
-    else:
-        print "未选择有效的run，结束。"
 
 if __name__ == "__main__":
-    run_bing()
+    bing_wow()
