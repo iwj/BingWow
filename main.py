cd49bfb12c6484c1befe940bd82db279568affae
Orange <jentlewoo@gmail.com>
2017-02-21 16:07:28 +0800

    start web version

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python
-# -*- coding:utf-8  -*-
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Date  : 2017-02-21
+# Author: juzi
+# E-mail: jentlewoo@gmail.com
 
 
-import bs4
-import requests
-import json
-import urllib
+import tornado.ioloop
+import tornado.web
+from handlers import *
 
-def bing_wow():
-    bing_json = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1444634662901&pid=hp"
-    result = requests.get(bing_json).text
-    result_dict = json.loads(result)
-    img_url = result_dict.get("images")[0].get("url")
-    img_url = "http://cn.bing.com" + img_url
-    file_name = img_url.split("/")[-1]
-    print img_url
-    print file_name
-    urllib.urlretrieve(img_url, file_name)
-
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
 
 if __name__ == "__main__":
-    bing_wow()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
